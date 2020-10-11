2daf14f2e50d626e2ad4be4f86d6e450cb7aeced
Anna C <anna.s.chong@gmail.com>
2020-10-11 13:03:04 +1100

    ADD navbar

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,28 @@
+// React libraries
+import React from 'react';
+
+// Material UI libraries
+import { AppBar, Toolbar, Typography, Button, Link } from '@material-ui/core';
+
+// Styles
+import { styles } from "./Nav.styles";
+
+
+
+export default function Nav() {
+  const classes = styles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Google Books
+          </Typography>
+          <Button color="inherit"><Link to="search">Search</Link></Button>
+          <Button color="inherit"><Link to="saved">Saved</Link></Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
