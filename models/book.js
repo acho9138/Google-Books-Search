969387acff1ebfb6b742747bbf347740a9c68344
Anna C <anna.s.chong@gmail.com>
2020-10-16 23:17:55 +1100

    UPDATE models

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   subtitle: String,
-  author: [{ type: String, required: true }],
+  authors: [{ type: String }],
   description: String,
   image: String,
   link: String
