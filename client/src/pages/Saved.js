0bd0c2f39aab78e36f320136e87707fa898e6029
Anna C <anna.s.chong@gmail.com>
2020-10-16 23:20:42 +1100

    ADD component

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,5 +1,5 @@
 // React libraries
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Material UI libraries
 import { Container, Box, Typography } from '@material-ui/core';
@@ -7,7 +7,31 @@ import { Container, Box, Typography } from '@material-ui/core';
 // Components
 import { Nav, Title, Form, BookResult } from "../components";
 
+// utils
+import API from "../utils/API";
+
+
 export const Saved = () => {
+  const [savedBooks, setSavedBooks] = useState([]);
+
+  const loadBooks = () => {
+    API.getSavedBooks()
+      .then(res => setSavedBooks(res.data))
+      .catch(err => console.log(err));
+  };
+
+  const deleteBook = (id) => {
+    API.deleteBook(id)
+      .then(() => {
+        loadBooks()
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    loadBooks()
+  }, [])
+
   return (
     <>
       <Nav />
@@ -24,7 +48,32 @@ export const Saved = () => {
           </Typography>
         </Box>
       </Container>
-      <BookResult isSave={false} />
+      {savedBooks.length ? (
+        savedBooks.map(book => {
+          return (
+            <BookResult
+              key={book._id}
+              isSave={false}
+              title={book.title}
+              subtitle={book.subtitle}
+              authors={book.authors}
+              description={book.description}
+              image={book.image}
+              link={book.link}
+              onClick={() => deleteBook(book._id)}
+            />
+          );
+        })
+      ) : (
+          <Container maxWidth="sm">
+            <Box component="span" m={1}>
+              <Typography variant="h5" gutterBottom>
+                No saved books.
+              </Typography>
+            </Box>
+          </Container>
+        )}
+
     </>
   )
 }
