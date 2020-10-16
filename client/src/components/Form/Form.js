2c696696063c0058adc6a24dd805b3298ed92cd5
Anna C <anna.s.chong@gmail.com>
2020-10-16 23:22:08 +1100

    UPDATE props

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -8,38 +8,39 @@ import { TextField, Grid, Button, Paper, Box } from '@material-ui/core';
 import { styles } from "./Form.styles";
 
 // Component
-export default function Form() {
+const Form = (props) => {
   const classes = styles();
 
   return (
-    <>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Paper elevation={3} className={classes.paper}>
-          <Grid container justify="center" spacing={3}>
-            <Grid item xs={12} sm={9}>
-              <form className={classes.form} noValidate autoComplete="off">
-                <TextField
-                  id="outlined-search"
-                  fullWidth
-                  label="Book Search"
-                  type="search"
-                  variant="outlined"
-                />
-              </form>
-            </Grid>
-            <Grid item xs={12} sm={2}>
-              <Button size="large" variant="contained" color="primary">
-                Search
-          </Button>
-            </Grid>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Paper elevation={3} className={classes.paper}>
+        <Grid container justify="center" spacing={3}>
+          <Grid item xs={12} sm={9}>
+            <form className={classes.form} autoComplete="off">
+              <TextField
+                id="outlined-search"
+                fullWidth
+                label="Book Search"
+                type="search"
+                variant="outlined"
+                onChange={props.handleInputChange}
+                value={props.value}
+              />
+            </form>
           </Grid>
-        </Paper>
-      </Box>
-
-    </>
+          <Grid item xs={12} sm={2}>
+            <Button onClick={props.onClick} size="large" variant="contained" color="primary">
+              Search
+              </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
   )
 }
+
+export default Form; 
